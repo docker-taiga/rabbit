ce3aaad798ae013894ff937dabac30396337625e
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2020-11-12 08:49:01 +0300

    Let rabbitmq:alpine handle default vhost and user

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM rabbitmq:alpine
 
 ENV RABBIT_USER=taiga \
-		RABBIT_PASSWORD=qwerty \
-		RABBIT_VHOST=taiga \
-    RABBITMQ_ERLANG_COOKIE=taiga \
-    RABBITMQ_PID_FILE=$RABBITMQ_DATA_DIR/rabbitmq.pid
+    RABBIT_PASSWORD=qwerty \
+    RABBIT_VHOST=taiga \
+    RABBITMQ_ERLANG_COOKIE=taiga
 
 COPY start.sh /start.sh
 
--- start.sh
+++ start.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
-exec docker-entrypoint.sh rabbitmq-server &
-ENTRYPOINT_PID=$!
+export RABBITMQ_DEFAULT_VHOST="$RABBIT_VHOST"
+export RABBITMQ_DEFAULT_USER="$RABBIT_USER"
+export RABBITMQ_DEFAULT_PASS="$RABBIT_PASSWORD"
 
-trap 'kill -TERM $ENTRYPOINT_PID' SIGTERM
-
-RABBIT_INITIALIZED_LOCK=/run/rabbit_initialized.lock
-if [ ! -f $RABBIT_INITIALIZED_LOCK ]; then
-    touch $RABBIT_INITIALIZED_LOCK
-
-    echo 'Waiting for rabbitmq to start...'
-    rabbitmqctl wait $RABBITMQ_PID_FILE
-
-    echo 'Initializing rabbitmq vhost and user...'
-    rabbitmqctl add_user $RABBIT_USER $RABBIT_PASSWORD
-    rabbitmqctl add_vhost $RABBIT_VHOST
-    rabbitmqctl set_permissions -p $RABBIT_VHOST $RABBIT_USER ".*" ".*" ".*"
-fi
-
-wait $ENTRYPOINT_PID
+exec docker-entrypoint.sh rabbitmq-server
